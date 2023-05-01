60582f919ec062e12dba3660b7ca7c28c70b9cdf
EmSabineCooke <117672124+EmSabineCooke@users.noreply.github.com>
2023-04-30 21:47:48 -0600

    Add files via upload

---
+++ juicebox-frontend/api/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+const { JWT_SECRET } = process.env;
+const apiRouter = express.Router();
+
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+
+  next();
+});
+
+apiRouter.use(async (req, res, next) => {
+  const prefix = 'Bearer';
+  const auth = req.header('Authorization');
+
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: 'Authorization Error'
+    });
+  }
+});
+
+const usersRouter = require('./users');
+const postsRouter = require('./posts');
+const tagsRouter = require('./tags');
+apiRouter.use('/users', usersRouter);
+apiRouter.use('/posts', postsRouter);
+apiRouter.use('/tags', tagsRouter);
+
+apiRouter.use((error, req, res, next) => {
+  res.send({
+    name: error.name,
+    messag: error.message
+  });
+});
+
+module.exports = apiRouter;
